860b6e76e888478c125f4cbda7d27bbc65825cb1
Scott D Phillips <scott.d.phillips@intel.com>
2018-01-09 20:34:43 -0800

    !UPSTREAM: i965/miptree: Map with movntdqa for linear buffers only
    Removes a place where gtt mapping is used.

--- src/mesa/drivers/dri/i965/intel_mipmap_tree.c
+++ src/mesa/drivers/dri/i965/intel_mipmap_tree.c
@@ -3543,7 +3543,8 @@ intel_miptree_map(struct brw_context *brw,
 #if defined(USE_SSE41)
    } else if (!(mode & GL_MAP_WRITE_BIT) &&
               !mt->compressed && cpu_has_sse4_1 &&
-              (mt->surf.row_pitch_B % 16 == 0)) {
+              (mt->surf.row_pitch_B % 16 == 0) &&
+              (mt->surf.tiling == ISL_TILING_LINEAR)) {
       intel_miptree_map_movntdqa(brw, mt, map, level, slice);
 #endif
    } else {

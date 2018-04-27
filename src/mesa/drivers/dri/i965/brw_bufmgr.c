dc2b878cfdfc272fce706222ce7eb9e1e9a83c62
Jordan Justen <jordan.l.justen@intel.com>
2018-04-27 15:29:14 -0700

    !UPSTREAM: fixup after softpin lands: i965: Add initial gen12 support
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/mesa/drivers/dri/i965/brw_bufmgr.c
+++ src/mesa/drivers/dri/i965/brw_bufmgr.c
@@ -1814,8 +1814,10 @@ brw_bufmgr_init(struct gen_device_info *devinfo, int fd, bool bo_reuse)
          /* Leave the last 4GB out of the high vma range, so that no state
           * base address + size can overflow 48 bits.
           */
+         const unsigned int bits = devinfo->gen >= 12 ? 47 : 48;
+         const uint64_t max_vma_size = MIN2(gtt_size, (1ull << bits));
          util_vma_heap_init(&bufmgr->vma_allocator[BRW_MEMZONE_OTHER],
-                            1 * _4GB, gtt_size - 2 * _4GB);
+                            1 * _4GB, max_vma_size - 2 * _4GB);
       } else if (devinfo->gen >= 10) {
          /* Softpin landed in 4.5, but GVT used an aliasing PPGTT until
           * kernel commit 6b3816d69628becb7ff35978aa0751798b4a940a in

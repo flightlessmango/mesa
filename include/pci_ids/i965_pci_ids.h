0155210646e6d71e76ca54331d10986ed45d824b
Jordan Justen <jordan.l.justen@intel.com>
2018-06-12 15:12:49 -0700

    intel: Add preliminary device info for gen 12.5
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- include/pci_ids/i965_pci_ids.h
+++ include/pci_ids/i965_pci_ids.h
@@ -260,4 +260,5 @@ CHIPSET(0x9A68, tgl_1x2x16, "Intel(R) HD Graphics (Tigerlake 1x2x16 GT1)")
 CHIPSET(0x9A70, tgl_1x2x16, "Intel(R) HD Graphics (Tigerlake 1x2x16 GT1)")
 CHIPSET(0x9A78, tgl_1x2x16, "Intel(R) HD Graphics (Tigerlake 1x2x16 GT1)")
 CHIPSET(0xFF20, tgl_1x6x16, "Intel(R) HD Graphics (Tigerlake GT2 Simulation)")
+CHIPSET(0x0201, tgl_2x4x16, "Intel(R) HD Graphics (Tigerlake ATS)")
 #endif

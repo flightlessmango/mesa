6c24927e7bc6f56b66f152b1849bdcd091480d09
Jordan Justen <jordan.l.justen@intel.com>
2018-10-30 11:57:33 -0700

    anv: Use compute blorp for vkCmdFillBuffer with INTEL_DEBUG=blocs
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/intel/vulkan/anv_blorp.c
+++ src/intel/vulkan/anv_blorp.c
@@ -882,9 +882,14 @@ void anv_CmdFillBuffer(
                                     MAX_SURFACE_DIM * bs, isl_format,
                                     &surf, &isl_surf);
 
+      const bool compute =
+         unlikely((INTEL_DEBUG & DEBUG_BLOCS) &&
+                  blorp_clear_supports_compute(batch.blorp, false, false,
+                                               surf.aux_usage));
+
       blorp_clear(&batch, &surf, isl_format, ISL_SWIZZLE_IDENTITY,
                   0, 0, 1, 0, 0, MAX_SURFACE_DIM, MAX_SURFACE_DIM,
-                  color, NULL, false);
+                  color, NULL, compute);
       fillSize -= max_fill_size;
       dstOffset += max_fill_size;
    }
@@ -899,8 +904,14 @@ void anv_CmdFillBuffer(
                                     MAX_SURFACE_DIM * bs, isl_format,
                                     &surf, &isl_surf);
 
+      const bool compute =
+         unlikely((INTEL_DEBUG & DEBUG_BLOCS) &&
+                  blorp_clear_supports_compute(batch.blorp, false, false,
+                                               surf.aux_usage));
+
       blorp_clear(&batch, &surf, isl_format, ISL_SWIZZLE_IDENTITY,
-                  0, 0, 1, 0, 0, MAX_SURFACE_DIM, height, color, NULL, false);
+                  0, 0, 1, 0, 0, MAX_SURFACE_DIM, height, color, NULL,
+                  compute);
       fillSize -= rect_fill_size;
       dstOffset += rect_fill_size;
    }
@@ -913,8 +924,13 @@ void anv_CmdFillBuffer(
                                     width * bs, isl_format,
                                     &surf, &isl_surf);
 
+      const bool compute =
+         unlikely((INTEL_DEBUG & DEBUG_BLOCS) &&
+                  blorp_clear_supports_compute(batch.blorp, false, false,
+                                               surf.aux_usage));
+
       blorp_clear(&batch, &surf, isl_format, ISL_SWIZZLE_IDENTITY,
-                  0, 0, 1, 0, 0, width, 1, color, NULL, false);
+                  0, 0, 1, 0, 0, width, 1, color, NULL, compute);
    }
 
    blorp_batch_finish(&batch);
--- src/intel/vulkan/genX_blorp_exec.c
+++ src/intel/vulkan/genX_blorp_exec.c
@@ -253,7 +253,10 @@ genX(blorp_exec)(struct blorp_batch *batch,
 
    genX(cmd_buffer_apply_pipe_flushes)(cmd_buffer);
 
-   genX(flush_pipeline_select_3d)(cmd_buffer);
+   if (params->compute_program)
+      genX(flush_pipeline_select_gpgpu)(cmd_buffer);
+   else
+      genX(flush_pipeline_select_3d)(cmd_buffer);
 
    genX(setup_aux_map)(cmd_buffer);
 

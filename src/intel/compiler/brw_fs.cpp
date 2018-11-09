bdc6924619d05630bc52d175fa1efc0c8c318e1b
Francisco Jerez <currojerez@riseup.net>
2018-11-09 14:13:37 -0800

    intel/fs: Fix constness of implied_mrf_writes() argument.

--- src/intel/compiler/brw_fs.cpp
+++ src/intel/compiler/brw_fs.cpp
@@ -1126,7 +1126,7 @@ fs_inst::flags_written() const
  * instruction -- the FS opcodes often generate MOVs in addition.
  */
 int
-fs_visitor::implied_mrf_writes(fs_inst *inst) const
+fs_visitor::implied_mrf_writes(const fs_inst *inst) const
 {
    if (inst->mlen == 0)
       return 0;

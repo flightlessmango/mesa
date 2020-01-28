fcde69372e6395e0614d045b056ca5627a854384
FlightlessMango <flightlessmangoyt@gmail.com>
2020-01-28 01:46:58 +0100

    Added explicit loader for mangohud

--- src/vulkan/overlay-layer/setup_mangohud.sh
+++ src/vulkan/overlay-layer/setup_mangohud.sh
@@ -3,17 +3,25 @@ MANGOHUD_DIR=$HOME/.local/share/MangoHud/
 LIB64=$HOME/.local/share/MangoHud/libMangoHud64.so
 LIB32=$HOME/.local/share/MangoHud/libMangoHud32.so
 IMPLICIT_LAYER_DIR=$HOME/.local/share/vulkan/implicit_layer.d 
+EXPLICIT_LAYER_DIR=$HOME/.local/share/vulkan/explicit_layer.d
 
 install() {
     mkdir -p $IMPLICIT_LAYER_DIR
+    mkdir -p $EXPLICIT_LAYER_DIR
     mkdir -p $MANGOHUD_DIR
     cp -v x32/libMangoHud32.so $MANGOHUD_DIR     
     cp -v x64/libMangoHud64.so $MANGOHUD_DIR
     cp -v implicit_layer.d/mangohud32.json $IMPLICIT_LAYER_DIR
     cp -v implicit_layer.d/mangohud64.json $IMPLICIT_LAYER_DIR
+    cp -v explicit_layer.d/mangohud32.json $EXPLICIT_LAYER_DIR
+    cp -v explicit_layer.d/mangohud64.json $EXPLICIT_LAYER_DIR
     sed -i "s|libMangoHud.so|$LIB32|g" $IMPLICIT_LAYER_DIR/mangohud32.json
     sed -i "s|libMangoHud.so|$LIB64|g" $IMPLICIT_LAYER_DIR/mangohud64.json
     sed -i "s|64bit|32bit|g" $IMPLICIT_LAYER_DIR/mangohud32.json
+    sed -i "s|libMangoHud.so|$LIB32|g" $EXPLICIT_LAYER_DIR/mangohud32.json
+    sed -i "s|libMangoHud.so|$LIB64|g" $EXPLICIT_LAYER_DIR/mangohud64.json
+    sed -i "s|64bit|32bit|g" $EXPLICIT_LAYER_DIR/mangohud32.json
+    sed -i "s|mangohud|mangohud32|g" $EXPLICIT_LAYER_DIR/mangohud32.json
 }
 
 uninstall() {
